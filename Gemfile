5f2dd4f9279788f4dbe814be6f914c25e26870fe
Chuong Nguyen <tinchuong@yahoo.fr>
2017-02-16 11:00:46 -0500

    basic version of microblogging app

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source "http://rubygems.org"
+gem "sinatra"
+gem "sinatra-flash"
+gem "activerecord"
+gem "sinatra-activerecord"
+
+group :development do 
+	gem "sqlite3"
+end
+
+group :production do 
+	gem "pg"
+end 
+
+gem "rake"
---
+++ main.rb
@@ -0,0 +1,81 @@
+require "sinatra"
+require "sinatra/activerecord"
+require "./models"
+require "sinatra/flash"
+# set :database, "sqlite3:test.sqlite3"
+configure(:development) {set :database, "sqlite3:test.sqlite3"}
+enable :sessions
+
+get "/" do
+	@users = User.all
+    @posts = Post.all
+    @user = User.find(session[:user_id]) if session[:user_id]
+    session[:visited] = "im here"
+    p session[:visited] 
+    erb :index
+end
+
+post "/posts" do
+    p params
+    # Post.create(title: params["title"],body: params["body"])
+
+    post = Post.new
+    post.title =params["title"]
+    post.body = params["body"]
+    post.save
+    redirect "/"
+
+end
+
+get "/posts/delete/:id" do
+    post = Post.find(params[:id])
+    post.destroy
+
+    redirect "/"
+end
+
+get "/posts/:id" do
+    @post = Post.find(params[:id])
+    erb :post_show
+end
+
+
+get "/posts/edit/:id" do
+    @post = Post.find(params[:id])
+    erb :post_edit 
+end
+
+
+post "/posts/edit/:id" do
+    @post = Post.find(params[:id])
+    @post.title = params[:title]
+    @post.body = params[:body]
+    @post.save
+    redirect "/posts/#{@post.id}"
+end
+
+get "/sign_in" do
+    erb :sign_in
+end
+
+get "/sign_out" do
+    session.clear
+    flash[:notice]="You are successfully logged out"
+    redirect "/"    
+end
+
+post "/sessions/new" do 
+    user = User.where(email: params[:email]).first
+    if user && user.password == params[:password]
+        session[:user_id] = user.id
+        flash[:notice]="You are successfully signed in"
+        redirect "/"
+    else 
+        flash[:notice]="Incorrect information"
+        redirect "/sign_in"
+    end  
+end 
+
+def current_user
+    @current_user = User.find(session[:user_id])
+end 
